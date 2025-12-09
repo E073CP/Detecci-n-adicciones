5957ad0f67cf7f4f49c420498ee236de49c0d7b6
cristiandaraithsanchezcarreon-coder <cristiandaraithsanchezcarreon@gmail.com>
2025-12-09 08:17:28 -0600

    Add files via upload
    este código contiene la version2.0 de nuestro software de detección de adicciones

---
+++ E7V2.181025.py
@@ -0,0 +1,11 @@
+#importar las bibliotecas 
+import tkinter as tk 
+#configurar la ventana 
+ventana=tk.Tk()
+ventana.title("E73CP TEST DE ADICCION A LAS REDES SOCIALES")# agregamos titulo a la ventana 
+ventana.geometry("1200x1100")#agregamos medidas a la ventana 
+ventana.config(bg="agregE7FEFD")# amos color a la ventana 
+
+
+#MANDAR A TRAER MI VENTANA
+ventana.mainloop()
